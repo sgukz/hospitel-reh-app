c67a36e7b2dcca170817b27b6c1ff72a28b73eba
sgukz <passathorn.dev@gmail.com>
2021-07-13 15:59:13 +0700

    update register

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 exports.main_config = {
   APP_URL: "https://api-reh-1meeting-forward.herokuapp.com/hospitel",
 };
+
 // exports.main_config = {
 //   APP_URL: "http://localhost:9000/hospitel",
 // };
